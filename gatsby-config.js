2f9a13b36e9360235da9cc523af7ff0e5243a3f3
timjkstrickland <timjkstrickland@gmail.com>
2019-12-10 21:04:30 -0800

    Updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,19 +3,23 @@ module.exports = {
     `gatsby-theme-blog`,
     `gatsby-theme-waves`
   ],
-  // Customize your site metadata:
+  
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `The War on Jank`,
+    author: `Tim JK Strickland`,
+    description: `This is a random assortment of web-related tutorials and thoughts. There might be other unrelated posts because I'm trying to shed the social media addiction.`,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/timjkstrickland`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/timjkstrickland`,
+      },
+      {
+        name: `codepen`,
+        url: `https://codepen.io/timjkstrickland`,
       },
     ],
   },
